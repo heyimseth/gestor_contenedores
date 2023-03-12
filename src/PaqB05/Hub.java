aee5b39111bef76b41a0a9a05ff32e2178dc704a
heyimseth <alvaromartinezalf@gmail.com>
2023-03-12 11:46:15 +0100

    Commit inicial con la definición de clases.

---
+++ src/PaqB05/Hub.java
@@ -0,0 +1,30 @@
+package PaqB05;
+
+/**
+ * Clase que define un hub de contenedores marítimos. Cada hub almacena hasta un total de 120 contenedores,
+ * apilados en 10 filas y 12 columnas.</br></br>
+ * La primera columna está destinada a contenedores con prioridad 1, la segunda columna a contenedores con
+ * prioridad 2, y el resto de columnas, a los contenedores con prioridad 3.
+ * @see Contenedor
+ */
+public class Hub {
+    private Contenedor[][] contenedores;    // debe inicializarse con 10 filas y 12 columnas.
+
+
+
+    /*######################################### Constructores ##############################################*/
+
+    /*######################################################################################################*/
+
+
+
+    /*####################################### Getters y setters ############################################*/
+
+    /*######################################################################################################*/
+
+
+
+    /*####################################### Métodos generales ############################################*/
+
+    /*######################################################################################################*/
+}
